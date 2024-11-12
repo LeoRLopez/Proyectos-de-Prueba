409b3e38cd489940cf691eef97fb8f41ec14e3d9
Leonardo Lopez <leonardo_lrl@hotmail.com>
2024-11-12 11:28:00 -0300

    Update PlayerList

--- AsociacionDeJugadores/src/components/PlayerList.tsx
+++ AsociacionDeJugadores/src/components/PlayerList.tsx
@@ -3,14 +3,14 @@ import { View, Text } from 'react-native';
 import { db } from '../../firebaseConfig';
 import { collection, onSnapshot, QuerySnapshot } from 'firebase/firestore';
 import { Player } from '../models/Player';
-import { useNavigation } from '@react-navigation/native';
+
 
 const PlayerList = () => {
     const [players, setPlayers] = useState<Player[]>([]);
 
     useEffect(() => {
         const unsubscribe = onSnapshot(
-            collection(db, 'players'),
+            collection(db, 'jugadores'),
             (snapshot: QuerySnapshot) => {
                 const playersData = snapshot.docs.map(doc => ({
                     id: doc.id,
